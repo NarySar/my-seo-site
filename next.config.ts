b4865ceb8fe9232ddd85b5ba89c9e2703e9f8fea
NarySar <chansovannary.sar001@umb.edu>
2026-01-22 19:06:48 -0500

    Initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',      // This creates the 'out' folder on build
+  images: {
+    unoptimized: true,   // Necessary because GitHub Pages lacks an image server
+  },
 };
 
 export default nextConfig;
