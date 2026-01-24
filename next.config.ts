4de1b7567a4b573e7cd86f55ed0e4a0da31f1c17
NarySar <chansovannary.sar001@umb.edu>
2026-01-23 22:25:34 -0500

    Remove static export to enable API

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',      // This creates the 'out' folder on build
-  images: {
-    unoptimized: true,   // Necessary because GitHub Pages lacks an image server
-  },
+  /* config options here */
 };
 
 export default nextConfig;
